6a198b06956ebabbf5eb9a8f296ed5a8ec1265ad
satnam72 <125819218+satnam72@users.noreply.github.com>
2025-08-23 06:11:41 +0000

    refactor: move variables under test

--- vitest.config.js
+++ vitest.config.js
@@ -5,15 +5,16 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-  },
-  environmentOptions: {
-    jsdom: { url: 'http://localhost/' },
-  },
-  restoreMocks: true,
-  clearMocks: true,
-  mockReset: true,
-  coverage: {
-    reporter: ['text', 'html'],
-    include: ['src/**/*.js'],
+
+    environmentOptions: {
+      jsdom: { url: 'http://localhost/' },
+    },
+    restoreMocks: true,
+    clearMocks: true,
+    mockReset: true,
+    coverage: {
+      reporter: ['text', 'html'],
+      include: ['src/**/*.js'],
+    },
   },
 });
