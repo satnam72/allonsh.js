d3137e58904eadc164e9a32406bfa7c968729992
satnam72 <125819218+satnam72@users.noreply.github.com>
2025-08-29 10:05:21 +0000

    chore: add ESLint with recommended rules and lint scripts

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': 'error',
+      'prefer-const': 'error'
+    }
+  }
+];
